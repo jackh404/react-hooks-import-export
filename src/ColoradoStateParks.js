20a95d742ca505eecaf68fe1ea64bc92be756eb4
Jack Henza <jackhenza@gmail.com>
2023-10-24 14:58:06 -0400

    fool around

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,18 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
-
+import MesaVerde from "./parks/MesaVerde"
+import * as RockyMountain from "./parks/RockyMountain"
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde />
+      <p>{RockyMountain.trees}
+      </p>
+    </>
+  )
 }
+
+export default ColoradoStateParks
